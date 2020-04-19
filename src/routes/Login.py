50fe0a9fb3006ad77159d8384ff8570cbd0c72cc
Alex Montgomery <alex.t.montgomery@gmail.com>
2020-04-18 20:51:33 -0700

    Updated Homepage, added links to Register page, implemented login and logout, added red messages on Register page to specify what should be changed.

--- src/routes/Login.py
+++ src/routes/Login.py
@@ -1,8 +1,10 @@
 from flask import render_template
 from flask import redirect
 from flask import flash
+from flask_login import current_user, login_user
 from src import app
 from src.forms import LoginForm
+from src.schemas import User
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,8 +19,17 @@ def login():
         Rendered template html
 
     """
+    #handles if someone types in this url when already logged in
+    if current_user.is_authenticated:
+        return redirect('/')
+    
     current_form = LoginForm()
+
     if current_form.validate_on_submit():
-        flash(f'Login requested for user {current_form.username.data}')
+        user = User.query.filter_by(username=current_form.username.data).first()
+        if user is None or not user.check_password(current_form.password.data):
+            flash('Invalid username or password')
+            return redirect('/login')
+        login_user(user, remember=current_form.remember_me.data)
         return redirect('/')
     return render_template('Login/login.html', title='Sign In', form=current_form)
