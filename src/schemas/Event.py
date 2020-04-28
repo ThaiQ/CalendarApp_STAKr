a9cf847ce737627de66a255a4ec6184207c34044
Fox_Zero <ngcuukhanh95@gmail.com>
2020-04-27 23:43:06 -0700

    updated Event.py

--- src/schemas/Event.py
+++ src/schemas/Event.py
@@ -1,22 +1,26 @@
-from src import db
-from src import login
 from datetime import datetime
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     """event's id"""
-    guest = db.Column(db.String(70), index=True, unique=True)
+    guest = db.Column(db.String(70), nullable=False)
+    """guest's name"""
     event_name = db.Column(db.String(100), nullable=False)
     """event's name"""
     event_description = db.Column(db.Text(2000), nullable=False)
     """event's description"""
-    event_date = db.Column(db.Date, nullable=False, default=date.utcnow)
-    event_month = db.Column(db.Month, nullable=False, default=month.utcnow)
-    event_year = db.Column(db.Year, nullable=False, default=year.utcnow)
-    start_hour = db.Column(db.Hour, nullable=False, default=time.utcnow)
-    end_hour = db.Column(db.Hour, nullable=False, default=time.utcnow)
-    """Event's datetime"""
+    event_date = db.Column(db.Date, nullable=False, date.utcnow)
+    """event's date"""
+    event_month = db.Column(db.Month, nullable=False, month.utcnow)
+    """event's month"""
+    event_year = db.Column(db.Year, nullable=False, year.utcnow)
+    """event's year"""
+    start_hour = db.Column(db.Hour, nullable=False, time.utcnow)
+    """event's start hour"""
+    end_hour = db.Column(db.Hour, nullable=False, time.utcnow)
+    """event's end hour"""
     length = db.Column(db.Integer)
+    """event's  durations"""
 
     def __repf__(self):
         return '<Event: {} {} {}>'.format(self.guest, self.event_name, self.event_description, self.event_month, self.event_name, self.event_year, self.length)
