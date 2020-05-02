a5e0607b47345d018b3b4e0f15f9d5a4e00eb9b4
Alex Montgomery <alex.t.montgomery@gmail.com>
2020-05-01 23:34:46 -0700

    Adding files before switch directory file location on local machine (backup)

--- src/forms/Settings.py
+++ src/forms/Settings.py
@@ -8,6 +8,8 @@ def minuteToAmPm(minute):
     hr = minute // 60
     min = minute % 60
     start_time = 9
+    if (min == 0) :
+        min = '00'
     if (start_time+total < 13) :
         return f'{int(start_time+total)}:{min} A.M.'
     return f'{int(start_time+total-12)}:{min} P.M.'
@@ -16,7 +18,7 @@ def initTimeRange():
     rangeChoices = []
     time = 0 #pretending 9am is at 0 value
     minute = 15 #each section of time is 15 minute
-    while minute*time < 13*60: #range is from 9am to 10pm ~ 13hrs to 13*60min
+    while minute*time <= 13*60: #range is from 9am to 10pm ~ 13hrs to 13*60min
         pair = (minute*time, minuteToAmPm(minute*time))
         rangeChoices.extend([pair])
         time = time + 1
