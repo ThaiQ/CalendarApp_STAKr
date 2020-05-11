852031274f161b7a50c0c37249ca647a7d44d258
ThaiQ <quachhongthai200@gmail.com>
2020-05-11 13:03:45 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config():
     SECRET_KEY = 'new-secret-key-for-cross-site-attack'
 
     #store data at the current directory folder
-    SQLALCHEMY_DATABASE_URI = 'postgres://ptdzzknfatdeot:56751518d0a222e3e56f60fb62ba9274afb71f8c8e8817a1a62c86c3e75dd423@ec2-50-17-90-177.compute-1.amazonaws.com:5432/dantgds9r370'
+    SQLALCHEMY_DATABASE_URI = 'postgres://dgxathrfwxaiet:9fb8f174a782b6ce57b450b980a17dc39acac79bda132fb40cbdfe660fe4b2b0@ec2-52-207-25-133.compute-1.amazonaws.com:5432/dd5egk237qbuus'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
