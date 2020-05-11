af67401504282bf41c481cdd74f36eb4e7f0a4ac
ThaiQ <quachhongthai200@gmail.com>
2020-05-11 05:47:55 -0700

    deploying db

--- Setup/setup.bat
+++ Setup/setup.bat
@@ -5,7 +5,9 @@ cd ~
 REM install dependencies
 WHERE pip3
 if %ERRORLEVEL% NEQ 1 (
-    call pip3 install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn
+    call pip3 install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn psycopg2 pipenv
+    call pipenv install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn psycopg2 pipenv
 ) else (
-    call pip install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn
+    call pip install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn psycopg2 pipenv
+    call pipenv install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn psycopg2 pipenv
 )
--- Setup/setup.sh
+++ Setup/setup.sh
@@ -5,7 +5,9 @@ cd ~
 # install dependencies
 if [ $"command -v pip3 | wc -l" > 0 ]
 then
-    pip3 install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn
+    pip3 install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn psycopg2 pipenv
+    pipenv install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn psycopg2 pipenv
 else 
-    pip install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn
+    pip install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn psycopg2 pipenv
+    pipenv install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn psycopg2 pipenv
 fi
