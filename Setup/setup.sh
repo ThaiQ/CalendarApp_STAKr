acad3d474ec90196654971108a59f23fd7838dcc
AMontgomery123 <alex.t.montgomery@gmail.com>
2020-05-10 19:15:29 -0700

    adding procfile, requirements, and updating setup

--- Setup/setup.sh
+++ Setup/setup.sh
@@ -5,7 +5,7 @@ cd ~
 # install dependencies
 if [ $"command -v pip3 | wc -l" > 0 ]
 then
-    pip3 install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator
+    pip3 install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn
 else 
-    pip install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator
+    pip install flask flask-wtf flask-sqlalchemy flask-login pytest sphinx wtforms_components email_validator gunicorn
 fi
