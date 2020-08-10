f252440c45c25d85575587acd46a5a1bf5c16f2a
ThaiQ <quachhongthai200@gmail.com>
2020-08-10 14:38:02 -0700

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config():
         2. on Heroku's Postgres server
     '''
     #store data at the current directory folder (local machine)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     #store data at Heroku's Postgres adds-on (in Heroku's settings)
-    #SQLALCHEMY_DATABASE_URI = 'postgres://hflqpmkxpzcbvj:af66596df83f5659fd3ba61f5ac29d34471e7c189259696f294913519caf695f@ec2-34-194-198-176.compute-1.amazonaws.com:5432/dc3i70uvlpldhu'
+    SQLALCHEMY_DATABASE_URI = 'postgres://hflqpmkxpzcbvj:af66596df83f5659fd3ba61f5ac29d34471e7c189259696f294913519caf695f@ec2-34-194-198-176.compute-1.amazonaws.com:5432/dc3i70uvlpldhu'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
